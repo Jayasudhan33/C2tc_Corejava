4af2a2ddf9433eb530947bc5972735f2fdd0b795
owner <owner@CISCO-LAB-18>
2024-07-23 14:40:05 +0530

    jayasudhan

---
+++ tnsifc39/src/com/tnsif/dayone/FirstProgram.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayone;
+//Demonstration of simple java program
+
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
+
